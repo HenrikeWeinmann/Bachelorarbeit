e0a0add252539b7622b59ce9687433c07227d002
Henrike Weinmann <henrike.weinmann@student.uni-tuebingen.de>
2021-04-29 20:27:10 +0200

    Added Layout but Alignment is not good

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
-#this is a test
+from PyQt6.QtGui     import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore    import *
+
 app = QApplication(sys.argv)
 qss = "Stylesheet.qss"
 
@@ -12,15 +14,43 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.createButton()
+        self.setGeometry(200,300,1000,600)
+        #Box links
+        box = QWidget()
+        box.setObjectName("box")
+        playbtn = QPushButton('')
+        playbtn.setObjectName("playbtn")
+        btn = QPushButton('test')
+        btn2 = QPushButton('test')
+        innerLeft = QVBoxLayout()
+        innerLeft.setAlignment(innerLeft,Qt.Alignment.AlignBottom)
+        innerLeft.addWidget(playbtn)
+        innerLeft.addWidget(btn)
+        innerLeft.addWidget(btn2)
+        box.setLayout(innerLeft)
 
+        #Box oben rechts
+        box2 = QWidget()
+        box2.setObjectName("box2")
+        #Box unten rechts
+        box3 = QWidget()
+        box3.setObjectName("box3")
+        grid3 = QGridLayout()
+        button2 = QPushButton('LOL')
+        button2.setMaximumWidth(100)
+        grid3.addWidget(button2)
+        box3.setLayout(grid3)
 
+        innerRight = QVBoxLayout()
+        innerRight.addWidget(box2)
+        innerRight.addWidget(box3)
 
-    def createButton(self):
-        button = QPushButton('hahahaha BUTTON',self)
+        #Layout
+        mainLayout = QHBoxLayout()
+        mainLayout.addWidget(box)
+        mainLayout.addLayout(innerRight)
 
-
-
+        self.setLayout(mainLayout)
 
 
 
